aa53f443ee3b8e79c9f38f98b77f79a0760037cd
unknown <vladncode@gmail.com>
2022-01-27 16:20:37 +0200

    fixed posts

--- posts/index.js
+++ posts/index.js
@@ -14,15 +14,13 @@ const posts = {};
 app.get('/posts', (req, res) => {
   res.send(posts);
 });
+
 app.post('/posts', (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = { id, title };
 
-  res.status(201).json({
-    status: 'success',
-    data: { post: posts[id] },
-  });
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
